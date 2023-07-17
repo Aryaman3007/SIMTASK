ecd3d5a7154a18cd756877f70f858ee063ec2844
Aryaman30 <111583885+Aryaman3007@users.noreply.github.com>
2023-07-17 20:34:46 +0530

    Update About.js

--- QuickCravers/src/About/About.js
+++ QuickCravers/src/About/About.js
@@ -7,10 +7,16 @@ const About = () => {
   return (
     <div>
       <div className="about-container">
-        <h3 className="text-center fw-bolder text-danger welcome-text">
+        <h2 className="text-center fw-bolder text-danger welcome-text">
           About QuickCravers!
-        </h3>
-
+        </h2>
+        <h5 className="text-center">
+        Welcome to QuickCravers, your ultimate food delivery app that brings delicious meals right to your doorstep! 
+        QuickCravers is a user-friendly mobile application that allows you to order your favorite meals from a wide range of 
+        local restaurants, cafes, and food vendors. With a few taps on your smartphone, you can explore various cuisines, 
+        view menus, customize orders, and have your food delivered quickly.
+        <span style={{display: "block", margin:"15px", color:"red"}}>Enjoy the convenience of QuickCravers and discover a world of flavors at your fingertips! Happy eating! 🍔🍕🍜</span>
+        </h5>
         <Button
           className="mt-3 btn-lg fw-bold d-block mx-auto py-2 px-4"
           style={{ backgroundColor: "rgb(198, 57, 57)", border: "none" }}
